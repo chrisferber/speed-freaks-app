4395e48de37fdcfca7af1472d28495bae6c66e44
Chris Ferber <chris@ferbers.us>
2019-11-29 21:33:20 -0700

    bring Material UI into project and create custom more info buttons on upcoming events page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,12 +17,29 @@ import AdminProtectedRoute from '../AdminProtectedRoute/AdminProtectedRoute';
 import UpcomingEvents from '../UpcomingEvents/UpcomingEvents';
 import Tracks from '../Tracks/Tracks';
 
+import 'typeface-roboto';
 
 import './App.css';
 import CreateEvent from '../CreateEvent/CreateEvent';
 import EventDetails from '../EventDetails/EventDetails';
 import Profile from '../Profile/Profile';
 import MyEvents from '../MyEvents/MyEvents';
+
+import {ThemeProvider as MuiThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: deepOrange,
+    secondary: blueGrey,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  }
+});
 
 class App extends Component {
   componentDidMount() {
@@ -31,59 +48,61 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/tracks" />
-            {/* Visiting localhost:3000/about will show the about page.
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+              <Redirect exact from="/" to="/tracks" />
+              {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/upcoming-events"
-              component={UpcomingEvents}
-            />
-            {/* For protected routes, the view could show one of several things on the same route.
+              <Route
+                exact
+                path="/upcoming-events"
+                component={UpcomingEvents}
+              />
+              {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
-              exact
-              path="/tracks"
-              component={Tracks}
-            />
-            {/* This works the same as the other protected route, except that if the user is logged in,
+              <ProtectedRoute
+                exact
+                path="/tracks"
+                component={Tracks}
+              />
+              {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <AdminProtectedRoute
-              exact
-              path="/create-event"
-              component={CreateEvent}
-            />
+              <AdminProtectedRoute
+                exact
+                path="/create-event"
+                component={CreateEvent}
+              />
 
-            <ProtectedRoute
-              exact
-              path="/event-details"
-              component={EventDetails}
-            />
+              <ProtectedRoute
+                exact
+                path="/event-details"
+                component={EventDetails}
+              />
 
-            <ProtectedRoute
-              exact
-              path="/profile"
-              component={Profile}
-            />
+              <ProtectedRoute
+                exact
+                path="/profile"
+                component={Profile}
+              />
 
-            <AdminProtectedRoute
-              exact
-              path="/my-events"
-              component={MyEvents}
-            />
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+              <AdminProtectedRoute
+                exact
+                path="/my-events"
+                component={MyEvents}
+              />
+              {/* If none of the other routes matched, we will show a 404. */}
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     )
   }
 }
