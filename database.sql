8373925544f647a4bee621eda6adc8a64458f2e8
Chris Ferber <chris@ferbers.us>
2019-12-10 10:44:22 -0600

    update postresql queries in database.sql file

--- database.sql
+++ database.sql
@@ -1,10 +1,32 @@
+-- Please copy and paste these sql queries to run is your postgresql database to get up and running.
+-- Database name is 'prime_app', if you would like to name your database something else you will need to change 'prime_app' to your new datbase name in server/modules/pool.js.
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "user"
+(
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (200) UNIQUE NOT NULL,
+    "is_admin" BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE "event"
+(
+    "id" SERIAL PRIMARY KEY,
+    "event_name" VARCHAR (300) UNIQUE NOT NULL,
+    "event_date_start" DATE,
+    "event_date_end" DATE,
+    "upcoming_description" TEXT NOT NULL,
+    "details_description" TEXT NOT NULL,
+    "admin_contact" VARCHAR (500) NOT NULL,
+    "created_id" INT,
+    "image_url" VARCHAR (300)
+);
+
+CREATE TABLE "user_event"
+(
+    "user_id" INT NOT NULL REFERENCES "user",
+    "event_id" INT NOT NULL REFERENCES "event",
+    "registration_complete" boolean NOT NULL DEFAULT false,
+        PRIMARY KEY ("user_id", "event_id")
 );
